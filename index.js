3b09304508191b3c9801cf8a371046dd006773b7
johnlemak <lemak91@mac.com>
2022-02-02 19:20:10 -0600

    commit tree d381f06f213b70424dbe9e20fa713d3bbdef2576

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
-// Write your code here
+// Write your code hered
+
+const num1 = 31
+const num2 = 2
+const num3 = 10
+const num4 = 6
+const mod = num3 % num4
+const multiply = num1 * num2
+
+const random = Math.ceil(Math.random() * 2)
+
+const max = Math.max(20,1, 6, 7, 4)
